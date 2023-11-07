8d7473c7b4bdbcdedf2e71efa0db6392fce65a6d
yash7xm <yash.7xm@gmail.com>
2023-11-07 22:05:29 +0530

    sidebar styles added

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -9,28 +9,32 @@ function Sidebar() {
             </div>
             <div className="buttons">
                 <div className="projects-btn btn">
-                    <div className="projects-btn-img">
-                        {/* <img src="src/assets/Images/img4.jpg" alt="" /> */}
-                    </div>
-                    <div className="btn-cont">
-                        <div className="btn-name">
-                            Projects
+                    <div className="btn-internal">
+                        <div className="projects-btn-img btn-img">
+                            <img src="https://framerusercontent.com/images/4tR2kkcmNDwWLyCTL6UKBxtGk.png" alt="" />
                         </div>
-                        <div className="btn-desc">
-                            My recent projects
+                        <div className="btn-cont">
+                            <div className="btn-name">
+                                Projects
+                            </div>
+                            <div className="btn-desc">
+                                My recent projects
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className="about-btn btn">
-                    <div className="about-btn-img">
-                        {/* <img src="src/assets/Images/img4.jpg" alt="" /> */}
-                    </div>
-                    <div className="btn-cont">
-                        <div className="btn-name">
-                            About
+                    <div className="btn-internal">
+                        <div className="about-btn-img btn-img">
+                            <img src="https://framerusercontent.com/images/4tR2kkcmNDwWLyCTL6UKBxtGk.png" alt="" />
                         </div>
-                        <div className="btn-desc">
-                            Let's get to know each other
+                        <div className="btn-cont">
+                            <div className="btn-name">
+                                About
+                            </div>
+                            <div className="btn-desc">
+                                Let's get to know each other
+                            </div>
                         </div>
                     </div>
                 </div>
