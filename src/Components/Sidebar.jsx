e59afd537b8f1f6940bf126af31c2c8e8b3c5e26
yash7xm <yash.7xm@gmail.com>
2023-11-07 12:38:26 +0530

    sidebar styles updated

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -8,7 +8,7 @@ function Sidebar() {
                 <div className="desc">Passionate computer engineering student excelling in programming, web development, and software engineering.</div>
             </div>
             <div className="buttons">
-                <div className="projects-btn">
+                <div className="projects-btn btn">
                     <div className="projects-btn-img">
                         {/* <img src="src/assets/Images/img4.jpg" alt="" /> */}
                     </div>
@@ -21,7 +21,7 @@ function Sidebar() {
                         </div>
                     </div>
                 </div>
-                <div className="about-btn">
+                <div className="about-btn btn">
                     <div className="about-btn-img">
                         {/* <img src="src/assets/Images/img4.jpg" alt="" /> */}
                     </div>
