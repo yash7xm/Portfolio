8504ffedf44d373e61c1a698109ebdfce664f899
yash7xm <yash.7xm@gmail.com>
2023-11-07 12:11:10 +0530

    sidebar content added

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -3,7 +3,38 @@ import '../assets/Styles/Sidebar.css'
 function Sidebar() {
     return (
         <div className="sidebar">
-            
+            <div className="info">
+                <div className="name">Yash Poonia</div>
+                <div className="desc">Passionate computer engineering student excelling in programming, web development, and software engineering. Proven leader in high-pressure team projects.</div>
+            </div>
+            <div className="buttons">
+                <div className="projects-btn">
+                    <div className="projects-btn-img">
+                        <img src="src/assets/Images/img4.jpg" alt="" />
+                    </div>
+                    <div className="btn-cont">
+                        <div className="btn-name">
+                            Projects
+                        </div>
+                        <div className="btn-desc">
+                            My recent projects
+                        </div>
+                    </div>
+                </div>
+                <div className="about-btn">
+                    <div className="about-btn-img">
+                        <img src="src/assets/Images/img4.jpg" alt="" />
+                    </div>
+                    <div className="btn-cont">
+                        <div className="btn-name">
+                            About
+                        </div>
+                        <div className="btn-desc">
+                            Let's get to know each other
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
