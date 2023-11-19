dfc4c89b9a8877f82ddf11dd4a38ec310ace7f4e
yash7xm <yash.7xm@gmail.com>
2023-11-19 12:32:14 +0530

    .

--- src/Components/ExpandProject.jsx
+++ src/Components/ExpandProject.jsx
@@ -5,7 +5,7 @@ function ExpandProject() {
         <div className="project-detail">
             <div className="main-img">
                 <div className="inner-img">
-                    <img src="src/assets/Images/img.png" alt="ProjectImage" />
+                    <img src="https://res.cloudinary.com/dgl5jcwgb/image/upload/v1700377042/img_afztvh.png" alt="ProjectImage" />
                 </div>
             </div>
 
