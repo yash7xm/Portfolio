af9ab8d08081ccc2a7e1e6f296bba44b6ff2e8b9
yash7xm <yash.7xm@gmail.com>
2023-11-25 16:15:41 +0530

    as0 added

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -8,18 +8,18 @@ function About() {
             </div>
             <div className="abt-content">
                 <div className="as0">
-                    <div className="abs0">
-                        <div className="my-img">
-                            
-                        </div>
-                        <div className="my-info">
-                            <div>Hi, I'm Yash</div>
-                            <div></div>
-                        </div>
+                    <div className="my-img">
+
                     </div>
+                    <div className="my-info">
+                        <div className='hd'>Hi, I'm Yash  👋🏼</div>
+                        <div className='bdy'>A Computer Engineering student passionate about programming language development, web tech, and software engineering. I'm focused on creating innovative projects and tackling coding challenges. Beyond academia, I've crafted a custom language, built an Integrated Learning Platform (ILP), and led the development of an interactive typing web app.</div>
+                        <div className="bdy">Outside the digital world, I enjoy exploring code conversion intricacies, participating in coding competitions, and constantly seeking growth opportunities. My commitment to excellence in academics and projects mirrors my passion for pushing the boundaries of what's possible in the tech realm. Let's connect and explore the limitless possibilities of coding together!</div>
+                    </div>
+
                 </div>
                 <div className="as1">
-                    
+
                 </div>
             </div>
         </div>
