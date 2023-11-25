979cc9df6fc88c6b548ce4cdc40499e9572d565a
yash7xm <yash.7xm@gmail.com>
2023-11-25 15:52:03 +0530

    about initial setup

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,7 +3,25 @@ import '../assets/Styles/About.css'
 function About() {
     return (
         <div className="about">
-            Abouttttttttttttttt
+            <div className="abt-btn">
+                About Me
+            </div>
+            <div className="abt-content">
+                <div className="as0">
+                    <div className="abs0">
+                        <div className="my-img">
+                            
+                        </div>
+                        <div className="my-info">
+                            <div>Hi, I'm Yash</div>
+                            <div></div>
+                        </div>
+                    </div>
+                </div>
+                <div className="as1">
+                    
+                </div>
+            </div>
         </div>
     )
 }
