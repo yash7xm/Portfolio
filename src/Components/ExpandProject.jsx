f2c42434fd2b0753293cdeeaef30ec4f38178b74
yash7xm <yash.7xm@gmail.com>
2023-12-05 01:35:09 +0530

    data fixed

--- src/Components/ExpandProject.jsx
+++ src/Components/ExpandProject.jsx
@@ -2,8 +2,11 @@ import "../assets/Styles/ExpandProject.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
+import data from "../utils/projectsData";
 
 function ExpandProject() {
+  const pData = data;
+  console.log(pData);
   return (
     <div className="project-detail">
       <Link to="/">
@@ -21,106 +24,64 @@ function ExpandProject() {
       <div className="project-detail-content">
         <div className="main-img">
           <div className="inner-img">
-            <img
-              src="https://res.cloudinary.com/dgl5jcwgb/image/upload/v1700902394/semmi_nnl2n0.png"
-              alt="ProjectImage"
-            />
+            <img src={pData[0].mainImg} alt="ProjectImage" />
           </div>
         </div>
 
         <div className="main-info">
           <div className="title">
-            <span>Vyzon</span> is a versatile and custom-tailored programming
-            language designed to provide coders with the freedom to shape their
-            coding experiences.
+            <span>{pData[0].pName}</span> {pData[0].mainInfo}
           </div>
 
           <div className="features">
             <div className="summary">
               <div className="head">Summary</div>
-              <div className="body">
-                Designed a custom programming language, built an interpreter,
-                and developed language-processing tools, showcasing language
-                development expertise.
-              </div>
+              <div className="body">{pData[0].body}</div>
             </div>
             <div className="tech-stack">
               <div className="head">Tech Stack</div>
-              <div className="body">Javascript</div>
+              {pData[0].techStack.map((tech, index) => (
+                <div key={index} className="body">
+                  {tech}
+                </div>
+              ))}
             </div>
             <div className="links">
               <div className="head">Links</div>
               <div className="body">
-                <a href="">[GitHub]</a>
-                <a href="">[Website]</a>
+                <a href={pData[0].gitLink}>[GitHub]</a>
+                <a href={pData[0].webLink}>[Website]</a>
               </div>
             </div>
           </div>
         </div>
         <div className="project-vid">
           <div className="vid">
-            <img
-              src="https://framerusercontent.com/images/0L4ByzyLonZaaYGw9F7w0kkuEo.gif"
-              alt=""
-            />
+            <img src={pData[0].vidLink} alt="" />
           </div>
         </div>
         <div className="project-text-0 project-text">
           <div className="text">
-            <div className="hd">PROMPT</div>
-            <div className="bdy mrb">
-              Global Citizen is a non-profit organization and action platform
-              with a mission to end extreme poverty by 2030. I was tasked to
-              redesign their mobile app to shift their overall product goal and
-              value proposition.
-            </div>
-            <div className="bdy mrb">
-              It’s a lofty goal to end extreme poverty with an app. It's also
-              difficult to successfully connect users to such a large global
-              movement in a digital space. Our team wanted to shift and simplify
-              the messaging of the Global Citizen app: a space where you can do
-              small things every day that positively impact yourself, your
-              community, and the world around you.
-            </div>
-            <div className="bdy mrb">
-              At the time, the GC app acted more as a passive content delivery
-              system and less as a place to facilitate user action and journey.
-              Our goal with this project was to redesign the app to encourage
-              daily action on critical global issues.
-            </div>
+            <div className="hd">{pData[0].projectText[0].head}</div>
+            <div className="bdy mrb">{pData[0].projectText[0].body[0]}</div>
+            <div className="bdy mrb">{pData[0].projectText[0].body[1]}</div>
+            <div className="bdy mrb">{pData[0].projectText[0].body[2]}</div>
           </div>
         </div>
 
         <div className="flow-chart">
           <img
-            src="https://res.cloudinary.com/dgl5jcwgb/image/upload/v1701714568/Untitled_1_xuir6o.jpg"
+            src= {pData[0].flowChart}
             alt=""
           />
         </div>
 
         <div className="project-text-0 project-text">
           <div className="text">
-            <div className="hd">PROMPT</div>
-            <div className="bdy mrb">
-              Global Citizen is a non-profit organization and action platform
-              with a mission to end extreme poverty by 2030. I was tasked to
-              redesign their mobile app to shift their overall product goal and
-              value proposition.
-            </div>
-            <div className="bdy mrb">
-              It’s a lofty goal to end extreme poverty with an app. It's also
-              difficult to successfully connect users to such a large global
-              movement in a digital space. Our team wanted to shift and simplify
-              the messaging of the Global Citizen app: a space where you can do
-              small things every day that positively impact yourself, your
-              community, and the world around you.
-            </div>
-            <div className="bdy mrb">
-              At the time, the GC app acted more as a passive content delivery
-              system and less as a place to facilitate user action and journey.
-              Our goal with this project was to redesign the app to encourage
-              daily action on critical global issues.
-            </div>
+            <div className="hd">{pData[0].projectText[1].head}</div>
+            <div className="bdy mrb">{pData[0].projectText[1].body[0]}</div>
+            <div className="bdy mrb">{pData[0].projectText[1].body[1]}</div>
+            <div className="bdy mrb">{pData[0].projectText[1].body[2]}</div>
           </div>
         </div>
       </div>
---
+++ src/utils/projectsData.js
@@ -0,0 +1,37 @@
+const data = [
+  {
+    pName: "Vyzon",
+    mainImg:
+      "https://res.cloudinary.com/dgl5jcwgb/image/upload/v1700902394/semmi_nnl2n0.png",
+    mainInfo:
+      "IS A VERSATILE AND CUSTOM-TAILORED PROGRAMMING LANGUAGE DESIGNED TO PROVIDE CODERS WITH THE FREEDOM TO SHAPE THEIR CODING EXPERIENCES.",
+    body: "Designed a custom programming language, built an interpreter, and developed language-processing tools, showcasing languagedevelopment expertise.",
+    techStack: [
+        "Javascript"
+    ],
+    gitLink: "/",
+    webLink: "/",
+    vidLink: "https://framerusercontent.com/images/0L4ByzyLonZaaYGw9F7w0kkuEo.gif",
+    projectText: [
+        {
+            head: "PROMPT",
+            body: [
+                "Global Citizen is a non-profit organization and action platform with a mission to end extreme poverty by 2030. I was tasked to redesign their mobile app to shift their overall product goal and value proposition.",
+                "It’s a lofty goal to end extreme poverty with an app. It's also difficult to successfully connect users to such a large global movement in a digital space. Our team wanted to shift and simplify the messaging of the Global Citizen app: a space where you can do small things every day that positively impact yourself, your community, and the world around you.",
+                "At the time, the GC app acted more as a passive content delivery system and less as a place to facilitate user action and journey. Our goal with this project was to redesign the app to encourage daily action on critical global issues."
+            ]
+        },
+        {
+            head: "SECOND",
+            body: [
+                "Global Citizen is a non-profit organization and action platform with a mission to end extreme poverty by 2030. I was tasked to redesign their mobile app to shift their overall product goal and value proposition.",
+                "It’s a lofty goal to end extreme poverty with an app. It's also difficult to successfully connect users to such a large global movement in a digital space. Our team wanted to shift and simplify the messaging of the Global Citizen app: a space where you can do small things every day that positively impact yourself, your community, and the world around you.",
+                "At the time, the GC app acted more as a passive content delivery system and less as a place to facilitate user action and journey. Our goal with this project was to redesign the app to encourage daily action on critical global issues."
+            ]
+        }
+    ],
+    flowChart: "https://res.cloudinary.com/dgl5jcwgb/image/upload/v1701714568/Untitled_1_xuir6o.jpg"
+  },
+];
+
+export default data;
